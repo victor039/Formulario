2355005394b6d5d2d85404b8651c2647f30b47a1
victor039 <tor-vic@hotmail.com>
2022-06-25 22:53:41 -0500

    Se modifican estilos del form

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,7 +1,17 @@
 "use strict"
+ document.addEventListener("DOMContentLoaded", function() {
+    M.AutoInit();
+  });
 
+
+
+/* Evento del boton del formulario Cabiar Id name
 const btn_send = document.getElementById("action");
-
 btn_send.addEventListener("click", function(){
     alert("EL formulario ha sido enviado.");
-});
+});*/
+
+
+
+
+ 
